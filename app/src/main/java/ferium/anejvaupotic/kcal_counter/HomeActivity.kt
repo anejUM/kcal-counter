676f60ad957de30d8944d289125cec085734b7b4
ResNeVem <anej.vaupotic1@student.um.si>
2021-03-27 15:36:47 +0100

    init commit

---
+++ app/src/main/java/ferium/anejvaupotic/kcal_counter/HomeActivity.kt
@@ -0,0 +1,11 @@
+package ferium.anejvaupotic.kcal_counter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.`activity_home.xml`)
+    }
+}
